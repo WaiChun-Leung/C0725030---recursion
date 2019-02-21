477aaeee05ac30158af472f3af8a40a8c3c405e8
waichun-leung <waichun.leung29@gmail.com>
2019-02-21 14:51:25 -0500

    Week 7 recursion commit

--- C0725030/Program.cs
+++ C0725030/Program.cs
@@ -10,6 +10,43 @@ namespace C0725030
     {
         static void Main(string[] args)
         {
+            //(1) Simple recursion
+            //new PlayWithRecursion().Add(3, 4);
+            new PlayWithRecursion();
+            Console.WriteLine(PlayWithRecursion.GetFactorial(4));
+        }
+    }
+    //(1) Simple recursion
+    class PlayWithRecursion
+    {
+        //simple recursion
+        public void Add(int a, int b)
+        {
+            //Have to add the step back to return to the back point
+            if (a > 10)
+            {
+                return;
+            }
+
+            Add(++a, --b);
+        }
+
+        public static long GetFactorial(int number)
+        {
+            Console.WriteLine(number);
+            //Condition
+            if (number == 0)
+            {
+                return 1;
+            }
+            //Call the method again
+            //return number is (int number)
+            //GetFactorial(number - 1) is the function of --> GetFactorial(int number)
+            return number * GetFactorial(number - 1);
         }
     }
 }
+
+
+//NOTE:
+//Everytime I call a method, I create a frame in the stack (call stack frame)
